9fecbc3a3f4fbe5d50244955c068cbf32e958689
perdislov <theglasstea@gmail.com>
2023-05-20 20:39:36 +0300

    Changed cpp file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include <string>
+ 
 using namespace std;
 
 int main(int argc, char** argv) {
-   cout << "Hello world" << endl;
+   string name;
+   cin >> name;
+   cout << "Hello world from " << name << endl;
 }
